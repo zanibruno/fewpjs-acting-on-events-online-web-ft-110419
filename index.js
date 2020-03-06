0d92f2b495e9c75ff1fcf86b63d556570e68ee51
zanibruno <corporate.zanin@gmail.com>
2020-03-06 18:14:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+      dodger.style.left = `${left - 2}px`
+    }
+  }
+  
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+      dodger.style.left = `${left + 2}px`
+    }
+  }
+
+  
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft()
+    }
+  })
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+  })
